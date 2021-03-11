d9ad6e8a740d89df3ecac75c122ac94466eabb72
DEKA-dev <51697924+DEKA-dev@users.noreply.github.com>
2021-03-11 16:31:27 +0100

    set variables

---
+++ app.js
@@ -0,0 +1,10 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButtton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-ac]')
+const previousOperand = document.querySelector('[data-p-operand]')
+const currentOprand = document-querySelector('[data-c-operand]')
+
+
+
