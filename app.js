b2b7d864b291a4171b42d26261b8f476772bb36f
DEKA-dev <51697924+DEKA-dev@users.noreply.github.com>
2021-03-11 17:25:17 +0100

    creating class calculator

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
+class Calculator {
+    constructor(previousOperand, currentOperand) {
+        this.previousOperand = previousOperand
+        this.currentOperand = currentOperand
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        this.currentOperand = number 
+    }
+
+    choseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperand.innerText = this.currentOperand
+    }
+}
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButtton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-ac]')
 const previousOperand = document.querySelector('[data-p-operand]')
-const currentOprand = document-querySelector('[data-c-operand]')
+const currentOperand = document.querySelector('[data-c-operand]')
 
 
+const calcualtor = new Calculator(previousOperand, currentOperand)
 
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calcualtor.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
